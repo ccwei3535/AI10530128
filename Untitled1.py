f40b6b9508939ea54c96c4dd565c2d16d01d5238
ccwei3535 <44254403+ccwei3535@users.noreply.github.com>
2018-10-18 14:55:21 +0800

    U1
    U1

---
+++ Untitled1.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+def function(n,x):
+    s=n
+    b=x
+    if b>s:
+        print("b is greater than a")
+    if b<s:
+        print("b is smaller than a")
+    if s==b:
+        print("s and b are equal")
+
+
+# In[4]:
+
+
+function(33,32);
+
